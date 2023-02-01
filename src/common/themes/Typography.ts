0834b2deee2f223d483512f2007da9d0239a70c1
schatzen <gomieti@naver.com>
2023-02-01 16:28:39 +0900

    Add: custom  typography setting

--- src/common/themes/Typography.ts
+++ src/common/themes/Typography.ts
@@ -3,38 +3,36 @@ import { TypographyOptions } from "@mui/material/styles/createTypography";
 const baseTypographyOptions: TypographyOptions = {
   fontFamily: "Pretendard",
   h1: {
-    fontSize: "150px",
+    fontSize: "60px",
+    lineHeight: "68px",
+    fontWeight: "bold",
   },
   h2: {
-    fontSize: "80px",
+    fontSize: "40px",
+    lineHeight: "48px",
+    fontWeight: "bold",
   },
   h3: {
-    fontSize: "60px",
+    fontSize: "35px",
+    lineHeight: "43px",
+    fontWeight: "bold",
   },
   h4: {
-    fontSize: "50px",
+    fontSize: "30px",
+    lineHeight: "38px",
+    fontWeight: "bold",
   },
 
   h5: {
-    fontSize: "44px",
-  },
-  h6: {
-    fontSize: "35px",
-  },
-
-  subtitle1: {
-    fontSize: "40px",
-  },
-  subtitle2: {
-    fontSize: "30px",
-  },
-  body2: {
-    fontSize: "20px",
+    fontSize: "25px",
+    lineHeight: "33px",
+    fontWeight: "normal",
   },
 
-  caption: {
-    fontSize: "24px",
-  },
+  body1: { fontSize: "18px", lineHeight: "26px", fontWeight: "normal" },
+  body2: { fontSize: "16px", lineHeight: "24px", fontWeight: "normal" },
+
+  caption: { fontSize: "14px", lineHeight: "22px", fontWeight: "normal" },
 };
 
 export const typographyOptions: TypographyOptions = {
