66c48f814cb4ace4212ec6a87fb38420a48c3a08
schatzen <gomieti@naver.com>
2023-02-02 15:26:09 +0900

    Feat: Appbar component 레이아웃 솔종 및 배치

---
+++ src/common/components/Appbar/Appbar.tsx
@@ -0,0 +1,8 @@
+import { Colors } from "@/common/themes/Color";
+import { Stack } from "@mui/material";
+
+export const Appbar = () => {
+  return (
+    <Stack sx={{ height: "89px", backgroundColor: Colors.BackBlue }}></Stack>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Layout } from "@/common/components/Layout";
 import "@/common/styles/globals.css";
 import { theme } from "@/common/themes/Theme";
 import { ThemeProvider } from "@mui/material";
@@ -6,7 +7,9 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 }
