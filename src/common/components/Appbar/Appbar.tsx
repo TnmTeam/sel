53d28f68b502c7ab0aaa7991ae0be65bb8fa01e8
JooYoung <jyjung@tnmtech.com>
2023-02-07 14:06:49 +0900

    Navigator Appbar 레이아웃 구현

--- src/common/components/Appbar/Appbar.tsx
+++ src/common/components/Appbar/Appbar.tsx
@@ -1,23 +1,160 @@
-import Link from 'next/link';
-import { css } from "@emotion/react";
-import { Colors } from "@/common/themes/Color";
-import { Stack } from "@mui/material";
+import { ClassNames, css } from '@emotion/react';
+import { Colors, NavigationItems, WhiteButtons } from '@/common/themes/Color';
+import {
+    Stack,
+    Box,
+    AppBar,
+    IconButton,
+    Typography,
+    Button,
+} from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import Image from 'next/image';
+import LogoImage from '@/assets/Logo/Frame.png';
+import ChatIcon from '@mui/icons-material/Chat';
 
 export const Appbar = () => {
-  return (
-    <Stack sx={{ height: "89px", backgroundColor: Colors.BackBlue, flexDirection: "row" }}>
-      <Link href="/login" css={st.link}>Login</Link>
-      <Link href="/overview" css={st.link}>Overview</Link>
-      <Link href="/progress" css={st.link}>Progress</Link>
-      <Link href="/account" css={st.link}>Account</Link>
-    </Stack>
-  );
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar
+                position='static'
+                sx={{ height: 89, background: Colors.BackBlue }}
+            >
+                <Toolbar component='div' sx={{ height: 89 }}>
+                    <Button href='/overview'>
+                        <Logo />
+                    </Button>
+                    <Typography
+                        variant='h6'
+                        component='div'
+                        sx={{ flexGrow: 1 }}
+                    ></Typography>
+
+                    <Button
+                        sx={{
+                            fontSize: '16pt',
+                            textTransform: 'none',
+                            marginRight: '40px',
+                            color: NavigationItems.TextColor,
+                            ':hover': {
+                                color: NavigationItems.OnHoverTextColor,
+                            },
+                        }}
+                        color='primary'
+                        href='/overview'
+                    >
+                        Overview
+                    </Button>
+                    <Button
+                        sx={{
+                            fontSize: '16pt',
+                            textTransform: 'none',
+                            marginRight: '40px',
+                            color: NavigationItems.TextColor,
+                            ':hover': {
+                                color: NavigationItems.OnHoverTextColor,
+                            },
+                        }}
+                        color='inherit'
+                        href='/progress'
+                    >
+                        Progress
+                    </Button>
+                    <Button
+                        sx={{
+                            fontSize: '16pt',
+                            textTransform: 'none',
+                            marginRight: '40px',
+                            color: NavigationItems.TextColor,
+                            ':hover': {
+                                color: NavigationItems.OnHoverTextColor,
+                            },
+                        }}
+                        color='inherit'
+                        href='/growth'
+                    >
+                        Growth
+                    </Button>
+                    <Button
+                        sx={{
+                            fontSize: '16pt',
+                            textTransform: 'none',
+                            marginRight: '40px',
+                            color: NavigationItems.TextColor,
+                            ':hover': {
+                                color: NavigationItems.OnHoverTextColor,
+                            },
+                        }}
+                        color='inherit'
+                        href='/resources'
+                    >
+                        Resources
+                    </Button>
+                    <Button
+                        sx={{
+                            fontSize: '18pt',
+                            backgroundColor: WhiteButtons.ButtonColor,
+                            color: WhiteButtons.TextColor,
+                            ':hover': {
+                                backgroundColor:
+                                    WhiteButtons.onHoverButtonColor,
+                                color: WhiteButtons.OnHoverTextColor,
+                                border: '0',
+                            },
+                            border: '0',
+                        }}
+                        href='/addinsights'
+                        variant='outlined'
+                    >
+                        Add Insights
+                    </Button>
+                    <IconButton
+                        size='large'
+                        edge='start'
+                        color='inherit'
+                        aria-label='menu'
+                        sx={{ mr: 2 }}
+                        href='/helpmenu'
+                    >
+                        <ChatIcon />
+                    </IconButton>
+
+                    <Button
+                        sx={{
+                            fontSize: '16pt',
+                            textTransform: 'none',
+                            marginRight: '40px',
+                            color: NavigationItems.TextColor,
+                            ':hover': {
+                                color: NavigationItems.OnHoverTextColor,
+                            },
+                        }}
+                        color='inherit'
+                        href='/account'
+                    >
+                        Account
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 };
 
 const st = {
-  link: css`
-    display: 'inline-block';
-    font-size: 20px;
-    margin-right: 20px;
-  `
-}
+    logoImage: css`
+        margin: 0px 0px 0px 0px;
+    `,
+};
+
+const Logo = () => {
+    return (
+        <Stack css={st.logoImage}>
+            <Image
+                objectFit='cover'
+                src={LogoImage}
+                alt={'img'}
+                width={'150'}
+            />
+        </Stack>
+    );
+};
--- src/common/themes/Color.ts
+++ src/common/themes/Color.ts
@@ -1,45 +1,85 @@
-import { PaletteOptions } from "@mui/material";
+import { PaletteOptions } from '@mui/material';
 
 export enum Colors {
-  // primary
-  FlexBlack = "#3D3935",
-  FlexRed = "#FF585D",
-  FlexGrey = "#F1F1F1",
-  FlexBlue = "#F7F9FD",
-  BackBlue = "#6787B7",
-  MedBlue = "#4A7199",
-  BottomBlue = "#617391",
+    // primary
+    FlexBlack = '#3D3935',
+    FlexRed = '#FF585D',
+    FlexGrey = '#F1F1F1',
+    FlexBlue = '#F7F9FD',
+    BackBlue = '#6787B7',
+    MedBlue = '#4A7199',
+    BottomBlue = '#617391',
 
-  // secondary
-  ActiveBlue = "#147AD6",
-  LateRed = "#EA4848",
-  CompeleteGreen = "#7ACB5E",
-  MiddleCompeleteYellow = "#EEB035",
+    // secondary
+    ActiveBlue = '#147AD6',
+    LateRed = '#EA4848',
+    CompeleteGreen = '#7ACB5E',
+    MiddleCompeleteYellow = '#EEB035',
 
-  // text
-  Text800 = "#282C36",
-  Text600 = "#4E5668",
-  Text400 = "#98A0B3",
-  Text200 = "#C3CAD9",
-  Text100 = "#F2F6FF",
-  BasicText = "#4F5B70",
+    // text
+    Text800 = '#282C36',
+    Text600 = '#4E5668',
+    Text400 = '#98A0B3',
+    Text200 = '#C3CAD9',
+    Text100 = '#F2F6FF',
+    BasicText = '#4F5B70',
 
-  // process color
-  UnCompeleteYellow = "#F1AB27",
+    // process color
+    UnCompeleteYellow = '#F1AB27',
+}
+
+// Flex Blue Buttons
+export enum FlexBlueButtons {
+    TextColor = '#FFFFFF',
+    ButtonColor = '#6787B7',
+    OnHoverTextColor = '#FFFFFF',
+    onHoverButtonColor = '#4E5668',
+}
+
+// White Buttons
+export enum WhiteButtons {
+    TextColor = '#6787B7',
+    ButtonColor = '#FFFFFF',
+    OnHoverTextColor = '#FFFFFF',
+    onHoverButtonColor = '#4E5668',
+}
+
+// Radial Buttons
+export enum RadialButtons {
+    TextColor = '#6787B7',
+    ButtonColor = '#6787B7',
+
+    OnHoverTextColor = '#6787B7',
+    OnHoverStrokeColor = '#EFF6FF',
+
+    SelectedTextColor = '#EFF6FF',
+    SelectedCircleColor = '#6787B7',
+}
+
+// Text Links
+export enum TextLinks {
+    TextColor = '#6787B7',
+}
+
+// Navigation Items
+export enum NavigationItems {
+    TextColor = '#C6CFE7',
+    OnHoverTextColor = '#FFFFFF',
+    SelectedTextColor = '#FFFFFF',
 }
 
 // 테마에 적용될 컬러 팔레트를 정의 합니다.
 export const palette: PaletteOptions = {
-  primary: {
-    main: Colors.FlexBlack,
-  },
-  secondary: {
-    main: Colors.ActiveBlue,
-  },
-  error: {
-    main: Colors.LateRed,
-  },
-  success: {
-    main: Colors.CompeleteGreen,
-  },
+    primary: {
+        main: Colors.FlexBlack,
+    },
+    secondary: {
+        main: Colors.ActiveBlue,
+    },
+    error: {
+        main: Colors.LateRed,
+    },
+    success: {
+        main: Colors.CompeleteGreen,
+    },
 };
