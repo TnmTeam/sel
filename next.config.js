9efa02626014f905bddc1c32f0f7fb701edb3e63
schatzen <gomieti@naver.com>
2023-02-08 19:14:26 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compiler: {
-        emotion: {
-            sourceMap: true,
-            autoLabel: 'dev-only',
-            labelFormat: '[dirname]-[filename]-[local]',
-        },
+  compiler: {
+    emotion: {
+      sourceMap: true,
+      autoLabel: "dev-only",
+      labelFormat: "[dirname]-[filename]-[local]",
     },
   },
+
   images: {
     unoptimized: true,
   },
