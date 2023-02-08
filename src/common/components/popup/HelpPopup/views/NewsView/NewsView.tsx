d034307cefb747a7d9d1ac08ab6faa5f7edcdbdf
schatzen <gomieti@naver.com>
2023-02-08 19:13:37 +0900

    Add: news 페이지 구현

---
+++ src/common/components/popup/HelpPopup/views/NewsView/NewsView.tsx
@@ -0,0 +1,35 @@
+import { css } from "@emotion/react";
+import { BottomBar } from "../../components";
+import { Content } from "./components";
+
+export const NewsView = () => {
+  return (
+    <div css={sx.root}>
+      <div css={sx.header}>News</div>
+      <Content />
+      <BottomBar />
+    </div>
+  );
+};
+
+const sx = {
+  header: css`
+    background: #6787b7;
+    color: #fff;
+    height: 64px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 700;
+    font-size: 18px;
+  `,
+  root: css`
+    width: 400px;
+    height: 682.18px;
+    border-radius: 16px;
+    position: relative;
+    font-size: 14px;
+    overflow: hidden;
+    box-shadow: 0px 5px 40px rgba(0, 0, 0, 0.16);
+  `,
+};
