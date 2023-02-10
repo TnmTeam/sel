bac39ef14525e36ef16af16540603bf5790e74dc
syahn <htama1@naver.com>
2023-02-10 16:29:46 +0900

    login image slider

--- src/domain/Login/sections/LoginSection/components/Previews/Previews.tsx
+++ src/domain/Login/sections/LoginSection/components/Previews/Previews.tsx
@@ -1,8 +1,17 @@
 import { css } from '@emotion/react';
 import { Grid } from '@mui/material';
 import { Colors } from '@/common/themes/Color';
+import SimpleImageSlider from 'react-simple-image-slider';
+
 
 export const Previews = () => {
+
+    const images = [
+        "/assets/login/slide1.png",
+        "/assets/login/slide2.png",
+        "/assets/login/slide3.png"
+    ]
+
     return (
         <Grid
             item
@@ -11,12 +20,29 @@ export const Previews = () => {
             md={6}
             sx={{
                 background: Colors.BackBlue,
-                backgroundImage: 'url(https://source.unsplash.com/random)',
-                backgroundRepeat: 'no-repeat',
+                // backgroundImage: 'url(https://source.unsplash.com/random)',
+                // backgroundRepeat: 'no-repeat',
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
             }}
-        ></Grid>
+        >
+            <SimpleImageSlider
+                width={"100%"}
+                height={"100%"}
+                style={{
+                    position: "relative",
+                    background: Colors.BackBlue,
+                    width: "80.86%",
+                    height: "98.93%",
+                    margin: "auto",
+                }}
+                images={images}
+                showNavs={true}        // left and right arrows
+                showBullets={true}     // Toggle Bullets
+                autoPlay={true}
+                autoPlayDelay={3}
+            />
+        </Grid>
     );
 };
 
