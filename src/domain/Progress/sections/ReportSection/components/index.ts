dabc8254aaffca1f35a071d42860c6d8f67eeb05
schatzen <gomieti@naver.com>
2023-02-10 22:43:17 +0900

    Update: Seggest 및 Report 섹션 추가

---
+++ src/domain/Progress/sections/ReportSection/components/index.ts
@@ -0,0 +1,3 @@
+export { StudentWorkbook } from "./StudentWorkbook";
+export { RecentUploads } from "./RecentUploads";
+export { ProgressReports } from "./ProgressReports";
--- src/domain/Progress/sections/index.ts
+++ src/domain/Progress/sections/index.ts
@@ -1,3 +1,4 @@
 export { BannerSection } from "./BannerSection/BannerSection";
 export { CourseSection } from "./CourseSection/CourseSection";
 export { ReportSection } from "./ReportSection/ReportSection";
+export { SuggestSection } from "./SuggestSection/SuggestSection";
