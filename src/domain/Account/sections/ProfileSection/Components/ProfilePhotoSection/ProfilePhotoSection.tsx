900707ca1de0f0635dcb27269ceb821ba947f150
hncho94 <vlfxhs94@gmail.com>
2023-02-10 19:37:56 +0900

    Update ProfilePhotoSection.tsx

--- src/domain/Account/sections/ProfileSection/Components/ProfilePhotoSection/ProfilePhotoSection.tsx
+++ src/domain/Account/sections/ProfileSection/Components/ProfilePhotoSection/ProfilePhotoSection.tsx
@@ -4,41 +4,6 @@ import { PhotoPopup } from "../../../EditPhotoSection/components";
 import { profileProps } from "../../ProfileSection";
 
 export const ProfilePhotoSection = ({photoTitle, photoUrl}:profileProps) => {
-    const StyledBadge = styled(Badge)(({ theme }) => ({
-        '& .MuiBadge-badge': {
-          backgroundColor: '#44b700',
-          color: '#44b700',
-          boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-          '&::after': {
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            borderRadius: '50%',
-            animation: 'ripple 1.2s infinite ease-in-out',
-            border: '1px solid currentColor',
-            content: '""',
-          },
-        },
-        '@keyframes ripple': {
-          '0%': {
-            transform: 'scale(.8)',
-            opacity: 1,
-          },
-          '100%': {
-            transform: 'scale(2.4)',
-            opacity: 0,
-          },
-        },
-      }));
-      
-      const SmallAvatar = styled(Avatar)(({ theme }) => ({
-        width: 22,
-        height: 22,
-        border: `2px solid ${theme.palette.background.paper}`,
-      }));
-
     return (
         <>
             <Stack
@@ -59,7 +24,6 @@ export const ProfilePhotoSection = ({photoTitle, photoUrl}:profileProps) => {
                 >
                     <Avatar alt="" src={photoUrl} sx={{ width: 120, height: 120 }} />
                 </Badge>
-                
             </Stack>
         </>
     );
