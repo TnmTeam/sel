8940e33825d198a3ba21e7a8f6351d26fe94e0d2
jspark <jspark@tnmtech.com>
2023-02-10 18:15:00 +0900

    Add: 사진 Drag&Drop 업로드

--- src/domain/Account/sections/EditPhotoSection/components/PhotoPopup/components/PhotoUpload/PhotoUpload.tsx
+++ src/domain/Account/sections/EditPhotoSection/components/PhotoPopup/components/PhotoUpload/PhotoUpload.tsx
@@ -1,17 +1,86 @@
 import { useState, useRef, useEffect } from 'react';
+import { css } from "@emotion/react";
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material';
 
 export const PhotoUpload = () => {
+    const [uploadedImage, setUploadedImage] = useState<File>();
+    const uploadBoxRef = useRef();
+
+    useEffect(() => {
+        const uploadBox = uploadBoxRef.current;
+        
+        const handleFiles = (file: File) => {
+            if(file === undefined) {
+                return;
+            }
+            
+            if(file.type.startsWith('image/')) {
+                const reader = new FileReader();
+                reader.onloadend = (e) => {
+                    const result = e.target!.result;
+                    setUploadedImage(result);
+                }
+                reader.readAsDataURL(file);
+            }
+        };
+        
+        const dropHandler = (event: any) => {
+            event.preventDefault();
+            event.stopPropagation();
+            const files = event.dataTransfer.files[0];
+            handleFiles(files);
+        };
+        
+        const dragOverHandler = (event: any) => {
+            event.preventDefault();
+            event.stopPropagation();
+        };
+        
+        uploadBox.addEventListener('drop', dropHandler);
+        uploadBox.addEventListener('dragover', dragOverHandler);
+
+        return () => {
+            uploadBox.removeEventListener('drop', dropHandler);
+            uploadBox.removeEventListener('dragover', dragOverHandler);
+        };
+      }, [uploadedImage]);
 
     return (
-        <Box sx={{ width: '280px', height: '280px', border: '1px dashed black', borderRadius: '140px', margin: 'auto', position: 'relative', marginTop: '30px', background: '#F7F7F7' }}>
-            <Typography component='h1' variant='h4' sx={{color: '#979797', position: 'absolute', top: '105px', left: 'calc(50% - 80px)'}}>
-                Drag Photo
-            </Typography>
-            <Typography component='h1' variant='h4' sx={{color: '#979797', position: 'absolute', top: '150px', left: 'calc(50% - 35px)'}}>
-                Here
-            </Typography>
+        <Box css={ st.photoBox } ref={uploadBoxRef}>
+            {
+                !!uploadedImage
+                    ? <div css={ st.imageBox }>
+                        <img src={`${ uploadedImage }`} />
+                    </div>
+                    : <>
+                        <Typography component='h1' variant='h4' sx={{color: '#979797', position: 'absolute', top: '105px', left: 'calc(50% - 80px)'}}>
+                            Drag Photo
+                        </Typography>
+                        <Typography component='h1' variant='h4' sx={{color: '#979797', position: 'absolute', top: '150px', left: 'calc(50% - 35px)'}}>
+                            Here
+                        </Typography>
+                    </>
+            }
         </Box>
     );
+}
+
+const st = {
+    photoBox: css`
+        width: 280px;
+        height: 280px;
+        border: 1px dashed black;
+        border-radius: 140px;
+        margin: auto;
+        position: relative;
+        margin-top: 30px;
+        background: #F7F7F7;
+    `,
+    imageBox: css`
+        width: 279px;
+        height: 279px;
+        overflow: hidden;
+        border-radius: 140px;
+    `
 }
