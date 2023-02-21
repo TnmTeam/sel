fd44c3d3b9d56be1c664d4c9db455ca07e6919ed
JooYoung <jyjung@tnmtech.com>
2023-02-21 18:48:05 +0900

    login 프로세스 수정

--- src/domain/Login/sections/LoginSection/components/Authentication/Authentication.tsx
+++ src/domain/Login/sections/LoginSection/components/Authentication/Authentication.tsx
@@ -21,12 +21,12 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GoogleIcon from '@mui/icons-material/Google';
 import AppleIcon from '@mui/icons-material/Apple';
 
-import { axiosClient } from "@/data/client/client";
+import { axiosClient } from '@/data/client/client';
 
 export const Authentication = () => {
     useEffect(() => {
         localStorage.removeItem('accessToken');
-      }, [])
+    }, []);
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -42,23 +42,160 @@ export const Authentication = () => {
         const provider = new GoogleAuthProvider(); // provider를 구글로 설정
         signInWithPopup(auth, provider) // popup을 이용한 signup
             .then(async (data) => {
+                console.log(data);
+                console.log(data.user);
                 const accessToken = await data.user.getIdToken();
                 // 만료되었거나 5분 이내 완료이면 새로 발급해오는 듯?
+
+                localStorage.setItme('studentInfo', data);
+
                 localStorage.setItem('accessToken', accessToken);
-                console.log('accessToken', accessToken);
-                location.href='/overview';
+                if (data.user.email != null)
+                    localStorage.setItem('email', await data.user.email);
+                if (data.user.displayName != null)
+                    localStorage.setItem(
+                        'displayName',
+                        await data.user.displayName
+                    );
+                if (data.user.displayName != null)
+                    localStorage.setItem('uid', await data.user.uid);
+                console.log('localStorage', localStorage);
+
+                // 로그인 API 연동
+                authLogin();
+                //location.href='/overview';
             })
             .catch((err) => {
                 console.log(err);
             });
     }
 
-    const apiTest = async () => {
-        console.log('hi, api test');
-        const response = await axiosClient('/reports/progress-reports/164');
-        console.log(response);
-    }
+    const authLogin = async () => {
+        const response = await axiosClient('/auth/login');
+        if (response.data.status_code == 500) {
+            // Expired toekn
+            console.log(response.data.message);
+        } else if (response.data.status_code == 400) {
+            // Account Does Not Exist
+            console.log(response.data.message);
 
+            // 자동으로 회원 가입( 2.23 임시)
+            authSignup();
+            // 회원 가입 페이지 자동 이동
+            //location.href='/signup';
+        } // 로그인 성공
+        else {
+            const authEmail = await response.data.email;
+            if (localStorage.getItem('email') == authEmail) {
+                // 회원 가입 성공
+                localStorage.setItem('role', await response.data.role);
+                location.href = '/overview'; // 페이지 이동
+            }
+        }
+    };
+
+    const authSignup = async () => {
+        const name = localStorage.getItem('displayName');
+        const email = localStorage.getItem('email');
+        const role = 'parent';
+        const password = '';
+        var params = {
+            name,
+            email,
+            role,
+            password,
+        };
+
+        // 자동으로 회원 가입( 2.23 임시)
+        const response = await axiosClient.post('/auth/signup', params);
+        console.log(response);
+
+        const authEmail = await response.data.email;
+        if (localStorage.getItem('email') == authEmail) {
+            // 회원 가입 성공
+            localStorage.setItem('role', await response.data.role);
+            location.href = '/overview'; // 페이지 이동
+        }
+
+        // 회원 가입 페이지 자동 이동 2.23 이후 제작 예정
+        // location.href='/signup';
+    };
+
+    const moveOverview = async () => {
+        localStorage.setItem('accessToken', '');
+        localStorage.setItem('email', 'bessietsang@hotmail.com');
+        localStorage.setItem('displayName', 'test');
+        localStorage.setItem('uid', '');
+        location.href = '/overview';
+    };
+
+    return (
+        <Grid item xs={12} sm={6} md={6} component={Paper} elevation={6} square>
+            <Box
+                flexDirection={'column'}
+                alignItems={'center'}
+                display={'flex'}
+                sx={{
+                    mx: 25,
+                    mt: 40,
+                    mb: 25,
+                }}
+            >
+                <Typography
+                    component='h1'
+                    variant='h3'
+                    textAlign='center'
+                    sx={{ mb: 3 }}
+                >
+                    Welcome Parents!
+                </Typography>
+
+                <Grid
+                    item
+                    flexDirection={'row'}
+                    alignItems={'center'}
+                    display={'flex'}
+                    sx={{ mb: 3 }}
+                >
+                    <Link href='#' onClick={handleGoogleLogin}>
+                        <Avatar
+                            sx={{
+                                m: 1,
+                                bgcolor: 'secondary.main',
+                                width: '50px',
+                                height: '50px',
+                            }}
+                        >
+                            <GoogleIcon />
+                        </Avatar>
+                    </Link>
+                </Grid>
+
+                <Button
+                    type='submit'
+                    fullWidth
+                    variant='contained'
+                    sx={{
+                        mt: 3,
+                        mb: 2,
+                        fontSize: '18pt',
+                        background: FlexBlueButtons.ButtonColor,
+                        color: FlexBlueButtons.TextColor,
+                        ':hover': {
+                            background: FlexBlueButtons.onHoverButtonColor,
+                            color: FlexBlueButtons.OnHoverTextColor,
+                        },
+                    }}
+                    onClick={moveOverview}
+                    //href='/overview'
+                >
+                    Login
+                </Button>
+            </Box>
+        </Grid>
+    );
+
+    /*
     return (
         <Grid item xs={12} sm={6} md={6} component={Paper} elevation={6} square>
             <Box
@@ -181,6 +318,7 @@ export const Authentication = () => {
             </Box>
         </Grid>
     );
+    */
 };
 
 const sx = {
