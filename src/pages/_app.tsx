772de4778c8abeb14e00b9d05921dff4ddea68f2
JooYoung <jyjung@tnmtech.com>
2023-02-23 13:52:08 +0900

    /select 화면 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,57 +1,49 @@
-import { Layout } from "@/common/components/Layout";
-import "@/common/styles/globals.css";
-import { theme } from "@/common/themes/Theme";
-import { ThemeProvider } from "@mui/material";
-import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { RecoilRoot } from "recoil";
+import { Layout } from '@/common/components/Layout';
+import '@/common/styles/globals.css';
+import { theme } from '@/common/themes/Theme';
+import { ThemeProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
+    defaultOptions: {
+        queries: {
+            retry: false,
+        },
     },
-  },
 });
 export default function App({ Component, pageProps }: AppProps) {
-  if (
-    (pageProps && pageProps.pathname) === "/" ||
-    (pageProps && pageProps.pathname) === "/login" ||
-    (pageProps && pageProps.pathname) === "/signup" ||
-    (pageProps && pageProps.pathname) === "/select"
-  ) {
     return (
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+            <RecoilRoot>
+                <ThemeProvider theme={theme}>
+                    {(pageProps && pageProps.pathname) === '/' ||
+                    (pageProps && pageProps.pathname) === '/login' ||
+                    (pageProps && pageProps.pathname) === '/signup' ||
+                    (pageProps && pageProps.pathname) === '/select' ? (
+                        <Component {...pageProps} />
+                    ) : (
+                        <Layout>
+                            <Component {...pageProps} />
+                        </Layout>
+                    )}
+                </ThemeProvider>
+            </RecoilRoot>
+        </QueryClientProvider>
     );
-  } else {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <ThemeProvider theme={theme}>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </ThemeProvider>
-        </RecoilRoot>
-      </QueryClientProvider>
-    );
-  }
 }
 
 App.getInitialProps = async (context: any) => {
-  const { ctx, Component } = context;
-  let pageProps = {};
+    const { ctx, Component } = context;
+    let pageProps = {};
 
-  if (Component.getInitialProps) {
-    // Component (pages 폴더에 있는 컴포넌트)에 getInitialProps가 있다면
-    pageProps = (await Component.getInitialProps(ctx)) || {};
+    if (Component.getInitialProps) {
+        // Component (pages 폴더에 있는 컴포넌트)에 getInitialProps가 있다면
+        pageProps = (await Component.getInitialProps(ctx)) || {};
 
-    return { pageProps };
-  } else {
-    return {};
-  }
+        return { pageProps };
+    } else {
+        return {};
+    }
 };
