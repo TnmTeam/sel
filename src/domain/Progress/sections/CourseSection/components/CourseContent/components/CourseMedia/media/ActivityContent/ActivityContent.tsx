a75cc489a1397abd74336d20b788e77cad924b5d
JooYoung <jyjung@tnmtech.com>
2023-03-14 18:36:07 +0900

    No Content text 처리

--- src/domain/Progress/sections/CourseSection/components/CourseContent/components/CourseMedia/media/ActivityContent/ActivityContent.tsx
+++ src/domain/Progress/sections/CourseSection/components/CourseContent/components/CourseMedia/media/ActivityContent/ActivityContent.tsx
@@ -55,7 +55,7 @@ export const ActivityContent = ({ selectedDetailCourse }: CourseMediaType) => {
                         ))}
                     </Stack>
                 ) : (
-                    <NoContent />
+                    <></>
                 )}
             </Stack>
         </Stack>
